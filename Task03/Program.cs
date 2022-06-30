5a3d08077ccc43fb08f83dc74f25938ed5cd6d9a
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-07-01 01:32:55 +0500

    add Task03 Cube

---
+++ Task03/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число больше 0: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Cube(n);
+
+void Cube(int n)
+{
+    if (n > 0)
+    {
+        int i = 1;
+        while (i <= n)
+        {
+            double cube = Math.Pow(i,3);
+            i++;
+            Console.WriteLine(cube);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите число больше 0!");
+    }
+}
