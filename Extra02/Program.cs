29c91932745b43db29e5fd5b19a33024e232b9c4
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-07-01 14:55:32 +0500

    add Extra02 Divide

---
+++ Extra02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дополнительные задачи.
+//Задача 2. Дано трёхзначное число N. Определить кратна ли трём сумма всех его цифр.
+
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Divide(n);
+
+void Divide(int n)
+{
+    if (n % 3==0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
