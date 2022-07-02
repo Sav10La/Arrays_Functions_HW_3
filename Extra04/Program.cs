879940818f79c933d5277d4ae6d06963a61635db
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-07-02 22:11:59 +0500

    add Extra04 ArrayOfTen

---
+++ Extra04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дополнительные задачи.
+//Задача 4. Дан массив длиной 10 элементов. Заполнить его последовательно числами от 1 до 10.
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    array[index] = 1;
+    while (index < length)
+    {
+        Console.WriteLine(array[index]);
+        index++;
+        array[index] = index + 1;
+    }
+}
+int[] array = new int [10];
+PrintArray(array);
